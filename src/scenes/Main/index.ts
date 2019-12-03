242eb12f23e9bf1b83b8afee895c8f6afc123459
liurongqing <phpgege@gmail.com>
2019-12-03 06:45:28 -0800

    优化速度与结束逻辑

--- src/scenes/Main/index.ts
+++ src/scenes/Main/index.ts
@@ -2,32 +2,34 @@ import { BASE_URL, PATH_URL } from '@/const'
 
 export default class Main extends Phaser.Scene {
   playerSprite: any // 主角精灵
-  treasureSprite: any // 宝箱精灵
+  goalSprite: any // 宝箱精灵
   enemiesSprite: any // 敌人精灵
   playerSpeed: number // 主角速度
-  enemySpeed: number // 敌人速度
+  enemyMinSpeed: number // 敌人最小速度
+  enemyMaxSpeed: number // 敌人最大速度
   enemyMaxY: number // 敌人范围值
   enemyMinY: number // 敌人范围值
-  isPlayerAlive: boolean // 主角是否还活着
+  isTerminating: boolean // 主角是否还活着
   constructor() {
     super('mainScene')
   }
 
   init() {
-    this.playerSpeed = 2
-    this.enemySpeed = 2
-    this.enemyMaxY = 280
+    this.playerSpeed = 3
+    this.enemyMinSpeed = 2
+    this.enemyMaxSpeed = 4.5
     this.enemyMinY = 80
-    this.isPlayerAlive = true
+    this.enemyMaxY = 280
+    this.isTerminating = false
   }
 
   preload() {
     this.load.setBaseURL(BASE_URL)
     this.load.setPath(PATH_URL)
     this.load.image('background', 'background.png') // 背景图
-    this.load.image('dragon', 'dragon.png') // 敌人
+    this.load.image('enemy', 'dragon.png') // 敌人
     this.load.image('player', 'player.png') // 主角
-    this.load.image('treasure', 'treasure.png') // 宝箱
+    this.load.image('goal', 'treasure.png') // 宝箱
   }
 
   create() {
@@ -39,25 +41,30 @@ export default class Main extends Phaser.Scene {
       .sprite(40, height * 0.5, 'player')
       .setScale(0.5)
 
-    this.treasureSprite = this.add
-      .sprite(width - 80, height * 0.5, 'treasure')
+    this.goalSprite = this.add
+      .sprite(width - 80, height * 0.5, 'goal')
       .setScale(0.6)
 
     this.enemiesSprite = this.add.group(undefined, {
-      key: 'dragon',
+      key: 'enemy',
       repeat: 5,
       setXY: {
-        x: 100,
+        x: 90,
         y: 100,
         stepX: 80,
         stepY: 20
       }
     })
-    Phaser.Actions.ScaleXY(this.enemiesSprite.getChildren(), -0.5, -0.5)
+    Phaser.Actions.ScaleXY(this.enemiesSprite.getChildren(), -0.4, -0.4)
     Phaser.Actions.Call(
       this.enemiesSprite.getChildren(),
       (enemy: any) => {
-        enemy.speed = Math.random() * 2 + 1
+        enemy.flipX = true
+        const direction = Math.random() < 0.5 ? 1 : -1
+        const speed =
+          this.enemyMinSpeed +
+          Math.random() * (this.enemyMaxSpeed - this.enemyMinSpeed)
+        enemy.speed = direction * speed
       },
       this
     )
@@ -65,40 +72,38 @@ export default class Main extends Phaser.Scene {
 
   // 游戏结束
   gameOver() {
-    this.isPlayerAlive = false
+    this.isTerminating = true
     this.cameras.main.shake(500)
-    this.time.delayedCall(
-      250,
+
+    this.cameras.main.on(
+      'camerashakecomplete',
       () => {
-        this.cameras.main.fade(250)
+        this.cameras.main.fade(500)
       },
-      [],
       this
     )
-    this.time.delayedCall(
-      500,
+
+    this.cameras.main.on(
+      'camerafadeoutcomplete',
       () => {
         this.scene.restart()
       },
-      [],
       this
     )
   }
 
   update() {
-    if (!this.isPlayerAlive) {
-      return
-    }
+    if (this.isTerminating) return
     if (this.input.activePointer.isDown) {
-      // console.log('111')
-      // this.player.x += this.playerSpeed
       this.playerSprite.x += this.playerSpeed
     }
 
+    const playerRect = this.playerSprite.getBounds()
+
     if (
       Phaser.Geom.Intersects.RectangleToRectangle(
-        this.playerSprite.getBounds(),
-        this.treasureSprite.getBounds()
+        playerRect,
+        this.goalSprite.getBounds()
       )
     ) {
       // 游戏成功
@@ -116,7 +121,7 @@ export default class Main extends Phaser.Scene {
 
       if (
         Phaser.Geom.Intersects.RectangleToRectangle(
-          this.playerSprite.getBounds(),
+          playerRect,
           enemies[i].getBounds()
         )
       ) {
